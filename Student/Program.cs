3553567151605bb6b126a331ed03f84a5d12ad2c
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2023-01-09 16:03:15 +0700

    first commit

---
+++ Student/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Student;
+
+Std student1 = new Std(18, 3.8, "Huynh Cong Chien");
+Console.WriteLine(student1);
---
+++ Student/Std.cs
@@ -0,0 +1,53 @@
+﻿namespace Student;
+
+public class Std:Object
+{
+    private int _age;
+    public int Age
+    {
+        get => _age; 
+        set {
+            if (_age < 0)
+            {
+                Console.WriteLine("Age must be greater than 0");
+                _age = 0;
+            }
+            else
+            {
+                _age = value;
+            }
+        }
+    }
+    private double _gpa;
+    public double Gpa
+    {
+        get => _gpa; 
+        set {
+            if (_gpa <= 0 || _gpa >= 4)
+            {
+                Console.WriteLine("GPA must be greater than 0 and smaller than 4");
+                _gpa = 0;
+            }
+            else
+            {
+                _gpa = value;
+            }
+        }
+    }
+    private string _name;
+    public string Name { get; set; }
+    
+    public Std() {}
+
+    public Std(int age, double gpa, string name)
+    {
+        _age = age;
+        _gpa = gpa;
+        _name = name;
+    }
+
+    public override string ToString()
+    {
+        return $"This is {_name} {_age} years old, have gpa {_gpa}.";
+    }
+}
